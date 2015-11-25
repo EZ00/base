4808fafc3aa246ce52fe92c9d032b673bc613d94
EZ <EZ_00@qq.com>
2015-11-25 18:01:59 +0800

    product 26

--- json/products.js
+++ json/products.js
@@ -1,5 +1,72 @@
 [
   {
+    "title":"cold rolled steel tube",
+    "number":0,
+    "currency":"USD",
+    "priceMin":450,
+    "priceMax":570,
+    "priceUnit":"ton/tons",
+    "moq":25,
+    "moqUnit":"ton/tons",
+    "supplyQuantity":6000,
+    "supplyUnit":"ton/tons",
+    "supplyPeriod":"month",
+    "consignmentTerm":"25 days after deposit",
+    "images":[
+      "05d0a6e8a538dbf739a52b98afcc6a436645efcdf57b0ff7c3f27f3df3cf46bc.jpg",
+      "9b6fe173da7f49fff965278228852456393a232458160d28dab37f600687b9ec.jpg",
+      "7647f682d3fa96bac1883169616c0f27b4afa9407f841db72a7a6d251d0092e2.jpg",
+      "51fcbced169452c03691193a668a40ca65209e5d820a44980af7526eb2a4889b.jpg",
+      "694c7060867b7175423dcf1ad2c65f26e573488f2b484609be38d5c71f23f48c.jpg",
+      "ab9adceb181bfd6bc1adb5939b23bf5875783a40a6cecfbc921702d0f5478c16.jpg"
+    ],
+    "kvs":{
+      "Steel grade":"Q195-Q235",
+      "Section shape":"square, rectangular, round, oval, LTZ",
+      "Surface treatment":"oiled",
+      "Technique":"cold rolled, ERW",
+      "Application":"Structure Pipe",
+      "Standard":"GB/T 13793-2008",
+      "Thickness":"0.35 - 2.0 mm",
+      "HS Code":"7306610000",
+      "Drawback":"9%",
+      "sizes":{
+        "Round":{
+          "outer diameter":"8-127mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        },
+        "Square":{
+          "side length":"10-100mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        },
+        "Rectangular":{
+          "axb":"6*10-50*120mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        },
+        "Stadium":{
+          "axb":"9*17-32*53mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        },
+        "Ellipse":{
+          "axb":"9*17-32*53mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        },
+        "Hexagon":{
+          "axb":"16*20mm",
+          "wall thickness":"mm",
+          "length":"mm"
+        }
+      }
+    },
+    "content":"",
+    "display":true
+  },
+  {
     "title":"cold rolled black annealed steel tube",
     "number":0,
     "currency":"USD",
