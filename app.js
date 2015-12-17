50e98075cca5f6034a7a30c84508afc4d9fa6547
EZ <EZ_00@qq.com>
2015-12-17 17:27:32 +0800

    fix bug: miss next()

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "http://127.0.0.1");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 };
 
 app.use(allowCrossDomain);
